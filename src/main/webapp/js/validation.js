a0c15c25167bb72707a65e42e5d43ff62f8fef69
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-05-19 00:13:19 +0300

    Add error_page.jsp / jsp package

--- src/main/webapp/js/validation.js
+++ src/main/webapp/js/validation.js
@@ -14,12 +14,10 @@ function validateLogin() {
     var loginRegex = '^[A-Za-z0-9_-]{3,30}$';
 
     if (login.match(loginRegex)) {
-        alert('login');
         document.getElementById('messageLogin').style.color = 'green';
         document.getElementById('messageLogin').innerHTML = '';
         return true;
     } else {
-        alert('wrong login')
         document.getElementById('messageLogin').style.color = 'red';
         document.getElementById('messageLogin').innerHTML = 'Login must contain 3 or more letter or numbers';
         return false;
@@ -31,12 +29,10 @@ function validateEmail() {
     var email = document.getElementById('user_email').value;
 
     if (emailRegex.test(email)) {
-        alert('mail');
         document.getElementById('messageEmail').style.color = 'green';
         document.getElementById('messageEmail').innerHTML = '';
         return true;
     } else {
-        alert('wrong email');
         document.getElementById('messageEmail').style.color = 'red';
         document.getElementById('messageEmail').innerHTML = 'Please set correct email';
         return false
@@ -48,12 +44,10 @@ function validatePhoneNumber() {
     var phoneNumber = document.getElementById('user_phone').value;
 
     if (phoneRegex.test(phoneNumber)) {
-        alert('phone');
         document.getElementById('messagePhone').style.color = 'green';
         document.getElementById('messagePhone').innerHTML = '';
         return true;
     } else {
-        alert('wrong phone');
         document.getElementById('messagePhone').style.color = 'red';
         document.getElementById('messagePhone').innerHTML = 'Wrong phone number format';
         return false
@@ -66,12 +60,10 @@ function validatePassword() {
     var repeatPassword = document.getElementById('password_repeat').value;
 
     if (passwordRegex.test(password) && passwordRegex.test(repeatPassword) && password === repeatPassword) {
-        alert('pass');
         document.getElementById('messagePassword').style.color = 'green';
         document.getElementById('messagePassword').innerHTML = '';
         return true;
     } else {
-        alert('wrong pass');
         document.getElementById('messagePassword').style.color = 'red';
         document.getElementById('messagePassword').innerHTML = 'Wrong password format';
         return false
