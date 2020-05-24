882f91d1aaeabfd9bec6f5848aa3330df66c53c6
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-05-24 19:19:15 +0300

    Add ConfirmUserOrderByAdminOnOrderSearchPageCommand class / command package

--- src/main/java/by/epam/javatraining/restaurant/command/impl/ConfirmUserOrderByAdminOnOrderSearchPageCommand.java
+++ src/main/java/by/epam/javatraining/restaurant/command/impl/ConfirmUserOrderByAdminOnOrderSearchPageCommand.java
@@ -7,45 +7,29 @@ import by.epam.javatraining.restaurant.entity.Order;
 import by.epam.javatraining.restaurant.exception.ServiceException;
 import by.epam.javatraining.restaurant.factory.ServiceFactory;
 import by.epam.javatraining.restaurant.service.OrderService;
-import by.epam.javatraining.restaurant.validator.EnterParameterValidator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class ConfirmUserOrderByAdminOnOrderSearchPageCommand implements Command {
     private static final Logger LOGGER = LogManager.getLogger(ConfirmUserOrderByAdminOnOrderSearchPageCommand.class);
     private static final int CONFIRMED_ORDER_STATUS_ID = 2;
-    private static final String WRONG_NUMBER_MESSAGE = "Not a number";
-    private static final String EMPTY_ORDER_MESSAGE = "Order with this id does not exist";
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        int orderId;
         Order order = null;
         OrderService service = ServiceFactory.INSTANCE.getOrderService();
-        List<Order> orderList = null;
+        int orderId = Integer.parseInt(request.getParameter(JSPParameter.ID.getValue()));
 
-        if (EnterParameterValidator.INSTANCE.isNumeric(request.getParameter(JSPParameter.ID.getValue()))) {
-            orderId = Integer.parseInt(request.getParameter(JSPParameter.ID.getValue()));
-            try {
-                order = service.getOrderById(orderId);
-                order.setOrderStatusId(CONFIRMED_ORDER_STATUS_ID);
-                service.updateOrder(order);
-            } catch (ServiceException e) {
-                LOGGER.error(e);
-            }
+        try {
+            order = service.getOrderById(orderId);
+            order.setOrderStatusId(CONFIRMED_ORDER_STATUS_ID);
+            service.updateOrder(order);
 
-        } else {
-            request.setAttribute(JSPParameter.ORDER_SEARCH_MESSAGE.getValue(), WRONG_NUMBER_MESSAGE);
+        } catch (ServiceException e) {
+            LOGGER.error(e);
         }
-
-        if (order == null) {
-            request.setAttribute(JSPParameter.ORDER_SEARCH_MESSAGE.getValue(), EMPTY_ORDER_MESSAGE);
-        }
-
-        request.setAttribute("orderRequest", order);
 
         return PageType.ADMIN_ORDER_SEARCH.getValue();
     }
